71e7aa7e4cd40cdc17e9b99c4b3ccb4713fabe81
Xage1 <kennethkiarie555@gmail.com>
2023-11-21 00:39:26 -0800

    ALX is Awesome

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
--- create database if missing
+-- create database hbnb_dev_db  if missing
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create user identified by password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant user all privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
